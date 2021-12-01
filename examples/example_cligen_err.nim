bc5b1fc1b4e610ad5e60cdc3e955e13fdd1d0f1e
Juan Carlos <juancarlospaco@gmail.com>
2021-12-01 10:21:51 -0800

    .

---
+++ examples/example_cligen_err.nim
@@ -0,0 +1,9 @@
+import std/macros, cligen
+block:
+  template a(foo = 'x') = discard
+  dispatch a
+block:
+  macro a(foo = 'x') = discard
+  dispatch a
+block:
+  dispatch (func(foo = 'x') = discard)
